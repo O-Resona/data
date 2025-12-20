480f2241b2eeff21946210a7269d81bfe9615c5c
O-Resona <3323149625@qq.com>
2025-12-20 13:11:10 +0800

    Update pick_data.js

--- pick_data.js
+++ pick_data.js
@@ -1,36 +1,772 @@
-player,NM1,NM2,NM3,NM4,NM5,NM6,HD1,HD2,HD3,HR1,HR2,HR3,DT1,DT2,DT3,FM1,FM2,FM3
-GAO HAO,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Moonlit111,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Flyer,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Tokirii,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Ruyaya,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Qualifiers,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Diaostrophism,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-DAQAO,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-aaront,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-SBHO,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Zkyler,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-13roil,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-AbbeyChan6203,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
--Whitedog-,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Silent Rock,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-7ony,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-lelxdme,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-My Angel Bronya,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-[ RoneKu ],0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
--Mqre-,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
--Inui Sana-,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Chloe Wing Kiu,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Bochi the osu,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-MeowHou,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Summer_Ouo,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-koutya_,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-oliwakami,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-RevengeWolf,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-gxytcgxytc,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-- kouyuu -,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-Fallen_HK,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+const pickData = [
+  {
+    player: "GAO HAO",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Moonlit111",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Flyer",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Tokirii",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Ruyaya",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Qualifiers",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Diaostrophism",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "DAQAO",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "aaront",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "SBHO",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Zkyler",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "13roil",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "AbbeyChan6203",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "-Whitedog-",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Silent Rock",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "7ony",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "lelxdme",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: true,    // NM3: 1
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: true,    // HR3: 1
+      675854: false,
+      34056: false,
+      299041: true,    // DT3: 1
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "My Angel Bronya",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: true,    // HR1: 1
+      775365: true,    // HR2: 1
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: true,    // DT3: 1
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "[ RoneKu ]",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: true,    // NM3: 1
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: true,    // HR1: 1
+      775365: true,    // HR2: 1
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "-Mqre-",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "-Inui Sana-",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Chloe Wing Kiu",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Bochi the osu",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "MeowHou",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Summer_Ouo",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "koutya_",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "oliwakami",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "RevengeWolf",
+    picks: {
+      698249: false,
+      202756: true,    // NM2: 1
+      863249: true,    // NM3: 1
+      611753: true,    // NM4: 1
+      279481: true,    // NM5: 1
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "gxytcgxytc",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: true,    // NM4: 1
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: true,    // HR3: 1
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "- kouyuu -",
+    picks: {
+      698249: true,    // NM1: 1
+      202756: false,
+      863249: false,
+      611753: true,    // NM4: 1
+      279481: false,
+      58970: false,
+      684163: true,    // HD1: 1
+      1118285: true,   // HD2: 1
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "Fallen_HK",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  },
+  {
+    player: "BurgerKing",
+    picks: {
+      698249: false,
+      202756: false,
+      863249: false,
+      611753: false,
+      279481: false,
+      58970: false,
+      684163: false,
+      1118285: false,
+      65233: false,
+      240488: false,
+      775365: false,
+      104389: false,
+      675854: false,
+      34056: false,
+      299041: false,
+      733432: false,
+      145669: false,
+      58063: false,
+      3276769: false
+    }
+  }
+];
 
-BurgerKing,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-
-
+export default pickData;
