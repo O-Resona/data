7821edd341177b2d6edfee0e31b64a5fd954e369
O-Resona <3323149625@qq.com>
2025-12-20 15:07:32 +0800

    Add files via upload

---
+++ bracket.js
@@ -0,0 +1,484 @@
+const SCHEDULE_DATA = {
+    "groupStage": [
+        {
+            "stage": "Group Stage",
+            "matchCode": "A1",
+            "date": "(Sun) Dec 28",
+            "time": "20:00",
+            "player1": "Diaostrophism",
+            "player2": "aaront",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "A2",
+            "date": "(Thu) Dec 18",
+            "time": "22:00",
+            "player1": "Moonlit111",
+            "player2": "AbbeyChan6203",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "A3",
+            "date": "(Tue) Dec 23",
+            "time": "21:00",
+            "player1": "aaront",
+            "player2": "AbbeyChan6203",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "A4",
+            "date": "(Sat) Dec 27",
+            "time": "21:00",
+            "player1": "Moonlit111",
+            "player2": "aaront",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "A5",
+            "date": "(Wed) Dec 24",
+            "time": "21:00",
+            "player1": "Diaostrophism",
+            "player2": "AbbeyChan6203",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "A6",
+            "date": "(Fri) Dec 26",
+            "time": "20:00",
+            "player1": "Moonlit111",
+            "player2": "Diaostrophism",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "B1",
+            "date": "(Sat) Dec 20",
+            "time": "22:00",
+            "player1": "Qualifiers",
+            "player2": "Zkyler",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "B2",
+            "date": "(Wed) Dec 24",
+            "time": "20:00",
+            "player1": "GAO HAO",
+            "player2": "Silent Rock",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "B3",
+            "date": "(Sun) Dec 21",
+            "time": "21:00",
+            "player1": "Zkyler",
+            "player2": "Silent Rock",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "B4",
+            "date": "(Sat) Dec 27",
+            "time": "20:00",
+            "player1": "GAO HAO",
+            "player2": "Zkyler",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "B5",
+            "date": "(Sat) Dec 27",
+            "time": "23:00",
+            "player1": "Qualifiers",
+            "player2": "Silent Rock",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "B6",
+            "date": "(Sun) Dec 28",
+            "time": "21:00",
+            "player1": "GAO HAO",
+            "player2": "Qualifiers",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "C1",
+            "date": "(Sat) Dec 20",
+            "time": "21:00",
+            "player1": "DAQAO",
+            "player2": "SBHO",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "C2",
+            "date": "(Sun) Dec 21",
+            "time": "20:00",
+            "player1": "Flyer",
+            "player2": "7ony",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "C3",
+            "date": "(Sun) Dec 21",
+            "time": "23:00",
+            "player1": "SBHO",
+            "player2": "7ony",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "C4",
+            "date": "(Sat) Dec 27",
+            "time": "19:00",
+            "player1": "Flyer",
+            "player2": "SBHO",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "C5",
+            "date": "(Sat) Dec 27",
+            "time": "21:00",
+            "player1": "DAQAO",
+            "player2": "7ony",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "C6",
+            "date": "(Sun) Dec 28",
+            "time": "22:00",
+            "player1": "Flyer",
+            "player2": "DAQAO",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "D1",
+            "date": "(Sat) Dec 20",
+            "time": "20:00",
+            "player1": "Ruyaya",
+            "player2": "13roil",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "D2",
+            "date": "(Sun) Dec 21",
+            "time": "21:00",
+            "player1": "Tokirii",
+            "player2": "-Whitedog-",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "D3",
+            "date": "(Sun) Dec 21",
+            "time": "22:00",
+            "player1": "13roil",
+            "player2": "-Whitedog-",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "D4",
+            "date": "(Sat) Dec 27",
+            "time": "18:00",
+            "player1": "Tokirii",
+            "player2": "13roil",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "D5",
+            "date": "(Sat) Dec 27",
+            "time": "20:00",
+            "player1": "Ruyaya",
+            "player2": "-Whitedog-",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "D6",
+            "date": "(Sun) Dec 28",
+            "time": "23:00",
+            "player1": "Tokirii",
+            "player2": "Ruyaya",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "E1",
+            "date": "(Sun) Dec 21",
+            "time": "21:30",
+            "player1": "MeowHou",
+            "player2": "RevengeWolf",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "E2",
+            "date": "(Sun) Dec 21",
+            "time": "21:00",
+            "player1": "My Angel Bronya",
+            "player2": "Fallen_HK",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "E3",
+            "date": "(Tue) Dec 23",
+            "time": "21:00",
+            "player1": "RevengeWolf",
+            "player2": "Fallen_HK",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "E4",
+            "date": "(Fri) Dec 19",
+            "time": "23:00",
+            "player1": "My Angel Bronya",
+            "player2": "RevengeWolf",
+            "redScore": 5,
+            "blueScore": 4
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "E5",
+            "date": "(Sun) Dec 28",
+            "time": "19:00",
+            "player1": "MeowHou",
+            "player2": "Fallen_HK",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "E6",
+            "date": "(Sun) Dec 28",
+            "time": "20:00",
+            "player1": "My Angel Bronya",
+            "player2": "MeowHou",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "F1",
+            "date": "(Mon) Dec 22",
+            "time": "21:00",
+            "player1": "Bochi the osu",
+            "player2": "koutya_",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "F2",
+            "date": "(Fri) Dec 19",
+            "time": "23:00",
+            "player1": "[ RoneKu ]",
+            "player2": "- kouyuu -",
+            "redScore": 2,
+            "blueScore": 5
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "F3",
+            "date": "(Sat) Dec 27",
+            "time": "13:51",
+            "player1": "koutya_",
+            "player2": "- kouyuu -",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "F4",
+            "date": "(Sat) Dec 27",
+            "time": "23:00",
+            "player1": "[ RoneKu ]",
+            "player2": "koutya_",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "F5",
+            "date": "(Sun) Dec 28",
+            "time": "18:00",
+            "player1": "Bochi the osu",
+            "player2": "- kouyuu -",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "F6",
+            "date": "(Sun) Dec 28",
+            "time": "21:00",
+            "player1": "[ RoneKu ]",
+            "player2": "Bochi the osu",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "G1",
+            "date": "(Sat) Dec 20",
+            "time": "18:00",
+            "player1": "-Inui Sana-",
+            "player2": "oliwakami",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "G2",
+            "date": "(Sat) Dec 20",
+            "time": "15:15",
+            "player1": "-Mqre-",
+            "player2": "BurgerKing",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "G3",
+            "date": "(Sat) Dec 20",
+            "time": "16:00",
+            "player1": "oliwakami",
+            "player2": "BurgerKing",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "G4",
+            "date": "(Sat) Dec 27",
+            "time": "13:00",
+            "player1": "-Mqre-",
+            "player2": "oliwakami",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "G5",
+            "date": "(Sat) Dec 20",
+            "time": "14:00",
+            "player1": "-Inui Sana-",
+            "player2": "BurgerKing",
+            "redScore": 2,
+            "blueScore": 5
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "G6",
+            "date": "(Sun) Dec 28",
+            "time": "22:00",
+            "player1": "-Mqre-",
+            "player2": "-Inui Sana-",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "H1",
+            "date": "(Sat) Dec 20",
+            "time": "23:30",
+            "player1": "Chloe Wing Kiu",
+            "player2": "Summer_Ouo",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "H2",
+            "date": "(Fri) Dec 19",
+            "time": "22:30",
+            "player1": "lelxdme",
+            "player2": "gxytcgxytc",
+            "redScore": 5,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "H3",
+            "date": "(Sun) Dec 21",
+            "time": "22:00",
+            "player1": "Summer_Ouo",
+            "player2": "gxytcgxytc",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "H4",
+            "date": "(Sat) Dec 27",
+            "time": "19:00",
+            "player1": "lelxdme",
+            "player2": "Summer_Ouo",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "H5",
+            "date": "(Sun) Dec 28",
+            "time": "18:00",
+            "player1": "Chloe Wing Kiu",
+            "player2": "gxytcgxytc",
+            "redScore": 0,
+            "blueScore": 0
+        },
+        {
+            "stage": "Group Stage",
+            "matchCode": "H6",
+            "date": "(Sun) Dec 28",
+            "time": "23:00",
+            "player1": "lelxdme",
+            "player2": "Chloe Wing Kiu",
+            "redScore": 0,
+            "blueScore": 0
+        }
+    ]
+};
