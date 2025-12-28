57299f45d49986f05c2fa6e72018885abb0d8f86
O-Resona <3323149625@qq.com>
2025-12-28 20:57:46 +0800

    Update pick_data.js

--- pick_data.js
+++ pick_data.js
@@ -98,16 +98,16 @@ const pickData = [
   {
     player: "Ruyaya",
     picks: {
-      698249: false,    // NM1: 0
+      698249: true,    // NM1: 1
       202756: false,    // NM2: 0
       863249: false,    // NM3: 0
       611753: false,    // NM4: 0
       279481: true,    // NM5: 1
       58970: true,    // NM6: 1
-      684163: false,    // HD1: 0
-      1118285: false,    // HD2: 0
+      684163: true,    // HD1: 1
+      1118285: true,    // HD2: 1
       65233: true,    // HD3: 1
-      240488: false,    // HR1: 0
+      240488: true,    // HR1: 1
       775365: false,    // HR2: 0
       104389: true,    // HR3: 1
       675854: false,    // DT1: 0
@@ -146,19 +146,19 @@ const pickData = [
   {
     player: "Diaostrophism",
     picks: {
-      698249: false,    // NM1: 0
+      698249: true,    // NM1: 1
       202756: false,    // NM2: 0
       863249: false,    // NM3: 0
       611753: false,    // NM4: 0
-      279481: false,    // NM5: 0
+      279481: true,    // NM5: 1
       58970: false,    // NM6: 0
       684163: false,    // HD1: 0
       1118285: true,    // HD2: 1
       65233: true,    // HD3: 1
-      240488: false,    // HR1: 0
+      240488: true,    // HR1: 1
       775365: false,    // HR2: 0
       104389: false,    // HR3: 0
-      675854: false,    // DT1: 0
+      675854: true,    // DT1: 1
       34056: false,    // DT2: 0
       299041: false,    // DT3: 0
       733432: true,    // FM1: 1
@@ -205,12 +205,12 @@ const pickData = [
       65233: false,    // HD3: 0
       240488: false,    // HR1: 0
       775365: false,    // HR2: 0
-      104389: false,    // HR3: 0
+      104389: true,    // HR3: 1
       675854: false,    // DT1: 0
       34056: false,    // DT2: 0
-      299041: false,    // DT3: 0
+      299041: true,    // DT3: 1
       733432: false,    // FM1: 0
-      145669: false,    // FM2: 0
+      145669: true,    // FM2: 1
       58063: false,    // FM3: 0
       3276769: false    // 保持0
     }
@@ -315,9 +315,9 @@ const pickData = [
     player: "-Whitedog-",
     picks: {
       698249: true,    // NM1: 1
-      202756: false,    // NM2: 0
+      202756: true,    // NM2: 1
       863249: true,    // NM3: 1
-      611753: false,    // NM4: 0
+      611753: true,    // NM4: 1
       279481: false,    // NM5: 0
       58970: true,    // NM6: 1
       684163: false,    // HD1: 0
@@ -325,11 +325,11 @@ const pickData = [
       65233: true,    // HD3: 1
       240488: true,    // HR1: 1
       775365: true,    // HR2: 1
-      104389: false,    // HR3: 0
+      104389: true,    // HR3: 1
       675854: false,    // DT1: 0
       34056: true,    // DT2: 1
       299041: false,    // DT3: 0
-      733432: false,    // FM1: 0
+      733432: true,    // FM1: 1
       145669: false,    // FM2: 0
       58063: true,    // FM3: 1
       3276769: false    // 保持0
@@ -438,7 +438,7 @@ const pickData = [
       202756: false,    // NM2: 0
       863249: true,    // NM3: 1
       611753: false,    // NM4: 0
-      279481: false,    // NM5: 0
+      279481: true,    // NM5: 1
       58970: false,    // NM6: 0
       684163: false,    // HD1: 0
       1118285: false,    // HD2: 0
@@ -448,7 +448,7 @@ const pickData = [
       104389: false,    // HR3: 0
       675854: false,    // DT1: 0
       34056: false,    // DT2: 0
-      299041: false,    // DT3: 0
+      299041: true,    // DT3: 1
       733432: false,    // FM1: 0
       145669: false,    // FM2: 0
       58063: false,    // FM3: 0
@@ -609,8 +609,8 @@ const pickData = [
       279481: false,    // NM5: 0
       58970: false,    // NM6: 0
       684163: true,    // HD1: 1
-      1118285: false,    // HD2: 0
-      65233: false,    // HD3: 0
+      1118285: true,    // HD2: 1
+      65233: true,    // HD3: 1
       240488: true,    // HR1: 1
       775365: true,    // HR2: 1
       104389: true,    // HR3: 1
@@ -618,7 +618,7 @@ const pickData = [
       34056: true,    // DT2: 1
       299041: false,    // DT3: 0
       733432: false,    // FM1: 0
-      145669: false,    // FM2: 0
+      145669: true,    // FM2: 1
       58063: false,    // FM3: 0
       3276769: false    // 保持0
     }
@@ -770,3 +770,4 @@ const pickData = [
 ];
 
 export default pickData;
+
